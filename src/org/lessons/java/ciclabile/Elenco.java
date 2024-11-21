4794e5f6f27cfd77d51b238271d521762882d341
Alessandro Ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-11-21 06:04:04 +0100

    Add full functionality and bonus features

--- src/org/lessons/java/ciclabile/Elenco.java
+++ src/org/lessons/java/ciclabile/Elenco.java
@@ -2,4 +2,44 @@ package org.lessons.java.ciclabile;
 
 public class Elenco {
 
+	private int[] elenco;
+	private int index;
+
+	public Elenco() {
+		this.elenco = new int[0];
+		this.index = 0;
+	}
+	
+	public Elenco(int[] elenco) {
+		this.elenco = elenco;
+		this.index = 0;
+	}
+	
+	public int getElementoSuccessivo() {
+		if (hasAncoraElementi()) {
+			return elenco[index++];
+		} else {
+			System.out.println("Non ci sono più elementi nell'elenco");
+			return 0;
+		}
+	}
+	
+	public boolean hasAncoraElementi() {
+		return index < elenco.length;
+	}
+	
+	public void addElemento(int elemento) {
+		int [] newElenco = new int [elenco.length + 1];
+		for(index = 0; index < elenco.length; index++) {
+			newElenco[index] = elenco [index];
+		}
+		newElenco[index] = elemento;
+		this.elenco = newElenco;
+		index = 0;
+		
+	}
+	
+	
+	
+	
 }
--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -3,8 +3,40 @@ package org.lessons.java.ciclabile;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] interi = {10,20,30,40,50,100};
+		
+		Elenco elencoInteri = new Elenco(interi);
+		System.out.println("Array in input ricevuto, inizio la lettura");
+		
+		while (elencoInteri.hasAncoraElementi()) {
+			System.out.println("Il prossimo elemento è: " + elencoInteri.getElementoSuccessivo());
+		}
+		
+		elencoInteri.addElemento(110);
+		System.out.println("\nAggiungo un elemento all'array e ripeto la lettura");
+		
+		while (elencoInteri.hasAncoraElementi()) {
+			System.out.println("Il prossimo elemento è: " + elencoInteri.getElementoSuccessivo());
+		}
+		
+		elencoInteri.addElemento(120);
+		System.out.println("\nAggiungo un elemento all'array e ripeto la lettura");
+		
+		while (elencoInteri.hasAncoraElementi()) {
+			System.out.println("Il prossimo elemento è: " + elencoInteri.getElementoSuccessivo());
+		}
+		
+		Elenco nuovoElenco = new Elenco();
+		System.out.println("\nInizializzo un nuovo array e mi preparo a ricevere il nuovo contenuto");
+		nuovoElenco.addElemento(15);
+		nuovoElenco.addElemento(25);
+		nuovoElenco.addElemento(35);
+		
+		System.out.println("\nValori ricevuti, inizio la lettura");
+		
+		while (nuovoElenco.hasAncoraElementi()) {
+			System.out.println("Il prossimo elemento è: " + nuovoElenco.getElementoSuccessivo());
+		}
 	}
 
 }
